befd156cdbc39ba3f6b87cd9cb2c307083ed9804
Keith Conger <keith.conger@gmail.com>
2014-01-24 14:36:24 -0500

    Major Rewrite to use AsyncTask

--- gen/org/thecongers/sdrweather/R.java
+++ gen/org/thecongers/sdrweather/R.java
@@ -27,9 +27,10 @@ public final class R {
         public static final int LinearLayout01=0x7f080000;
         public static final int TextView01=0x7f080001;
         public static final int TextView02=0x7f080002;
-        public static final int action_settings=0x7f080005;
-        public static final int button1=0x7f080004;
+        public static final int action_settings=0x7f080006;
+        public static final int button1=0x7f080005;
         public static final int button2=0x7f080003;
+        public static final int button3=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,6 +43,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int header=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int refresh=0x7f050006;
         public static final int start=0x7f050004;
         public static final int stop=0x7f050005;
     }
--- src/org/thecongers/sdrweather/MainActivity.java
+++ src/org/thecongers/sdrweather/MainActivity.java
@@ -1,63 +1,44 @@
 package org.thecongers.sdrweather;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 import android.view.Menu;
-import android.view.View;
 import android.widget.TextView;
 
 import com.stericson.RootTools.*;
 
 public class MainActivity extends Activity {
-	
-	private static final String TAG = "MainActivity";
-	TextView view;
-	Process nativeApp;
-	
-	// File Copy Function
-	private static void copyFile(String assetPath, String localPath, Context context) {
-	    try {
-	        InputStream in = context.getAssets().open(assetPath);
-	        FileOutputStream out = new FileOutputStream(localPath);
-	        int read;
-	        byte[] buffer = new byte[4096];
-	        while ((read = in.read(buffer)) > 0) {
-	            out.write(buffer, 0, read);
-	        }
-	        out.close();
-	        in.close();
-	        Log.d(TAG, "File " + assetPath + " copied successfully.");
-
-	    } catch (IOException e) {
-	        throw new RuntimeException(e);
-	    }
-	}
-
+    RtlTask mTask;
+    private static final String TAG = "SDRWeather";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    	
-    	super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    	
-    	// Create a File object for the parent directory
-    	File nativeDirectory = new File("/data/data/org.thecongers.sdrweather/nativeFolder/");
-    	// have the object build the directory structure, if needed.
+        // String dataRoot = getAppContext().getFilesDir().getParentFile().getPath();
+        String dataRoot = "/data/data/org.thecongers.sdrweather";
+        String binDir = dataRoot + "/nativeFolder/";
+    	// Create directory for binaries
+    	File nativeDirectory = new File(binDir);
     	nativeDirectory.mkdirs();
     	// Copy binaries
-    	copyFile("nativeFolder/multimon","/data/data/org.thecongers.sdrweather/nativeFolder/multimon",getBaseContext());
-    	copyFile("nativeFolder/rtl_fm","/data/data/org.thecongers.sdrweather/nativeFolder/rtl_fm",getBaseContext());
+    	copyFile("nativeFolder/multimon",dataRoot + "/nativeFolder/multimon",getBaseContext());
+    	copyFile("nativeFolder/rtl_fm",dataRoot + "/nativeFolder/rtl_fm",getBaseContext());
     	// Set execute permissions
         StringBuilder command = new StringBuilder("chmod 700 ");
-        command.append("/data/data/org.thecongers.sdrweather/nativeFolder/multimon");
+        command.append(dataRoot + "/nativeFolder/multimon");
         try {
 			Runtime.getRuntime().exec(command.toString());
 		} catch (IOException e3) {
@@ -66,7 +47,7 @@ public class MainActivity extends Activity {
 		}
         // Set execute permissions
         StringBuilder command2 = new StringBuilder("chmod 700 ");
-        command2.append("/data/data/org.thecongers.sdrweather/nativeFolder/rtl_fm");
+        command2.append(dataRoot + "/nativeFolder/rtl_fm");
         try {
 			Runtime.getRuntime().exec(command2.toString());
 		} catch (IOException e3) {
@@ -74,75 +55,113 @@ public class MainActivity extends Activity {
 			e3.printStackTrace();
 		}
     }
-    
-    // When Start button is touched
-    public void onClickStart(View view) {
-        // Check for root
-        if (RootTools.isRootAvailable()) {
-        	Process nativeApp = null;
-        	//Start
-        	try {
-        		String[] cmd = { "/system/xbin/su", "-c", "/data/data/org.thecongers.sdrweather/nativeFolder/rtl_fm -N -f 162.546M -s 22.5k -g 50 | /data/data/org.thecongers.sdrweather/nativeFolder/multimon -a EAS -q -t raw -" };
-        		//String[] cmd = { "/system/xbin/su", "-c", "/data/data/org.thecongers.sdrweather/nativeFolder/multimon" };
-        		nativeApp = Runtime.getRuntime().exec(cmd);
-        	} catch (IOException e3) {
-        		// TODO Auto-generated catch block
-        		e3.printStackTrace();
-        	}
-        	//End
-        	view =  (TextView) findViewById(R.id.TextView02);
-        	BufferedReader reader = new BufferedReader(new InputStreamReader(nativeApp.getInputStream()));
-        	int read;
-        	char[] buffer = new char[4096];
-        	StringBuffer output = new StringBuffer();
-        	
-        	try {
-        		//String line;
-        		//while ((line = reader.readLine()) != null) {
-        	    //    System.out.println(line); 
-        	    //    view.append(line);
-        	    //}
-        		while ((read = reader.read(buffer)) > 0) {
-        			output.append(buffer, 0, read);
-        			((TextView) view).append(output.toString());
-        		}
-        		reader.close();
-        	} catch (IOException e1) {
-        		// TODO Auto-generated catch block
-        		e1.printStackTrace();
-        	}
-
-        	// Waits for the command to finish.
-        	//try {
-        	//	nativeApp.waitFor();
-        	//} catch (InterruptedException e) {
-        		// TODO Auto-generated catch block
-        	//	e.printStackTrace();
-        	//}
-
-        	//Display Output
-        	//String nativeOutput =  output.toString();
-        	//TextView view =  (TextView) findViewById(R.id.TextView02);
-        	//view.setText(nativeOutput);
-        } else {
-        	String error = "No SU!";
-        	view =  (TextView) findViewById(R.id.TextView02);
-        	((TextView) view).setText(error);
-        	RootTools.offerSuperUser(MainActivity.this);
-        }
-        
-    }
-    // When Stop button is pressed
-    public void onClickStop(View view) {
-    	nativeApp.destroy();
-    }
-
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        mTask = new RtlTask();
+        mTask.execute();
     }
-    
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mTask.stop();
+    }
+
+    class RtlTask extends AsyncTask<String, Void, Void> {
+        PipedOutputStream mPOut;
+        PipedInputStream mPIn;
+        LineNumberReader mReader;
+        Process mProcess;
+        TextView mText = (TextView) findViewById(R.id.TextView02);
+        @Override
+        protected void onPreExecute() {
+            mPOut = new PipedOutputStream();
+            try {
+                mPIn = new PipedInputStream(mPOut);
+                mReader = new LineNumberReader(new InputStreamReader(mPIn));
+            } catch (IOException e) {
+                cancel(true);
+            }
+
+        }
+
+        public void stop() {
+            Process p = mProcess;
+            if (p != null) {
+                p.destroy();
+            }
+            cancel(true);
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+		Log.d(TAG, "Excuting rtl_fm/multimon");
+		String[] cmd = { "/system/xbin/su", "-c", "/data/data/org.thecongers.sdrweather/nativeFolder/rtl_fm -N -f 162.546M -s 22.5k -g 50 | /data/data/org.thecongers.sdrweather/nativeFolder/multimon -a EAS -q -t raw -" };
+		//String[] cmd = { "/system/xbin/su", "-c", "/data/data/org.thecongers.sdrweather/nativeFolder/multimon" };
+                mProcess = new ProcessBuilder()
+                    .command(cmd)
+                    .redirectErrorStream(true)
+                    .start();
+
+                try {
+                    InputStream in = mProcess.getInputStream();
+                    OutputStream out = mProcess.getOutputStream();
+                    byte[] buffer = new byte[1024];
+                    int count;
+
+                    // in -> buffer -> mPOut -> mReader -> 1 line of ping information to parse
+                    while ((count = in.read(buffer)) != -1) {
+                        mPOut.write(buffer, 0, count);
+                        publishProgress();
+                    }
+                    out.close();
+                    in.close();
+                    mPOut.close();
+                    mPIn.close();
+                } finally {
+                    mProcess.destroy();
+                    mProcess = null;
+                }
+            } catch (IOException e) {
+            }
+            return null;
+        }
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            try {
+                // Is a line ready to read from the command?
+                while (mReader.ready()) {
+                    // This just displays the output, you should typically parse it I guess.
+                    mText.setText(mReader.readLine());
+                    Log.d(TAG, "Output: " + mReader.readLine());
+                    if (mReader.readLine().contains("EAS")) {
+        				Log.d(TAG, "Found EAS Alert");
+        		    }
+                    
+                }
+            } catch (IOException t) {
+            }
+        }
+    }
+    // File Copy Function
+ 	private static void copyFile(String assetPath, String localPath, Context context) {
+ 	    try {
+ 	        InputStream in = context.getAssets().open(assetPath);
+ 	        FileOutputStream out = new FileOutputStream(localPath);
+ 	        int read;
+ 	        byte[] buffer = new byte[4096];
+ 	        while ((read = in.read(buffer)) > 0) {
+ 	            out.write(buffer, 0, read);
+ 	        }
+ 	        out.close();
+ 	        in.close();
+ 	        Log.d(TAG, "File " + assetPath + " copied successfully.");
+
+ 	    } catch (IOException e) {
+ 	        throw new RuntimeException(e);
+ 	    }
+ 	}
 }
