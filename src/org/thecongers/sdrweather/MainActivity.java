5004b28609d1998119993df8fcbcba66df5b42e4
Keith Conger <keith.conger@gmail.com>
2014-02-03 22:36:57 -0500

    * Added settings menu * Lookup and set frequency spinner * Country setting for location code lookups

--- src/org/thecongers/sdrweather/MainActivity.java
+++ src/org/thecongers/sdrweather/MainActivity.java
@@ -17,11 +17,16 @@ import java.util.TimeZone;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -34,9 +39,13 @@ public class MainActivity extends Activity {
     private static final String TAG = "SDRWeather";
     private Cursor events;
     private Cursor fips;
+    private Cursor clc;
     private EventDatabase eventdb;
     private FipsDatabase fipsdb;
+    private ClcDatabase clcdb;
     private Spinner spinner1;
+    private SharedPreferences sharedPrefs;
+    private static final int SETTINGS_RESULT = 1;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         eventdb = new EventDatabase(this);
         fipsdb = new FipsDatabase(this);
+        clcdb = new ClcDatabase(this);
+        
+        //Set Initial Frequency From Preferences
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int freq = Integer.parseInt(sharedPrefs.getString("prefDefaultFreq", "6"));
+        Log.d(TAG, "Freq set to: " + freq );
+        spinner1.setSelection(freq);
         
         // Get data root
         String dataRoot = getApplicationContext().getFilesDir().getParentFile().getPath();
@@ -68,13 +85,13 @@ public class MainActivity extends Activity {
 
     public void onClickStart(View view) {
     	if (RootTools.isRootAvailable()) {
-    		// Get Frequency
-    		spinner1 = (Spinner) findViewById(R.id.spinner1);
+    		//Get Frequency
     		String freq = String.valueOf(spinner1.getSelectedItem());
     		// Call for process to start
     		mTask = new RtlTask();
     		mTask.execute(freq);
     	} else {
+    		// Display message about lack of root
     		Toast.makeText(MainActivity.this,
     				"Root Not Available!",
     					Toast.LENGTH_SHORT).show();
@@ -98,6 +115,53 @@ public class MainActivity extends Activity {
         
     }
 	*/
+    // Draw Options Menu
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+    // When Settings Menu is selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+            case R.id.action_settings:
+                // Settings Menu was selected
+            	Log.d(TAG, "Settings Menu was selected");
+            	Intent i = new Intent(getApplicationContext(), UserSettingActivity.class);
+                startActivityForResult(i, SETTINGS_RESULT);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+    	}
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+                super.onActivityResult(requestCode, resultCode, data);
+
+                    if(requestCode==SETTINGS_RESULT)
+                    {
+                        updateUserSettings();
+                    }
+
+    }
+    
+    private void updateUserSettings() 
+    {
+
+    	 Toast.makeText(MainActivity.this,
+    			"Settings Changed!",
+    			Toast.LENGTH_SHORT).show();
+         
+         int freq = Integer.parseInt(sharedPrefs.getString("prefDefaultFreq", "6"));
+         Log.d(TAG, "Freq set to: " + freq );
+         spinner1.setSelection(freq);
+
+     }
+
+    
     class RtlTask extends AsyncTask<String, Void, Void> {
         PipedOutputStream mPOut;
         PipedInputStream mPIn;
@@ -140,7 +204,7 @@ public class MainActivity extends Activity {
             	String dataRoot = getApplicationContext().getFilesDir().getParentFile().getPath();
             	Log.d(TAG, "Got data root: "+ dataRoot);
             	Log.d(TAG, "Frequency Selected: "+ params[0]);
-            	//String[] cmd = { "/system/xbin/su", "-c", dataRoot + "/nativeFolder/rtl_fm -N -f "+ params[0] + "M -s 22.5k -g 50 | " + dataRoot + "/nativeFolder/multimon -a EAS -q -t raw -" };
+            	//String[] cmd = { "/system/xbin/su", "-c", dataRoot + "/nativeFolder/rtl_fm -N -f " + params[0] + "M -s 22.5k -g 50 | " + dataRoot + "/nativeFolder/multimon -a EAS -q -t raw -" };
             	//String[] cmd = { "/system/xbin/su", "-c", dataRoot + "/nativeFolder/rtl_fm -N -f 162.546M -s 22.5k -g 50 | " + dataRoot + "/nativeFolder/multimon -a EAS -q -t raw -" };
             	String[] cmd = { "/system/xbin/su", "-c", dataRoot + "/nativeFolder/multimon" };
                 mProcess = new ProcessBuilder()
@@ -238,22 +302,44 @@ public class MainActivity extends Activity {
         				easMsg[size - 3] = temp[0];
         				int j=0;
         				String [] locationCodes = new String[size - 5];
-        				countiesText.setText("Counties Affected: ");
-        				for (int i=3; i < size - 2; i++) {
-        					locationCodes[j] = easMsg[i];
-        					Log.d(TAG, "Location Code: " + locationCodes[j]);
+        				
+        				countiesText.setText("Regions Affected: ");
+        				//Get Country From Preferences
+        		        int country = Integer.parseInt(sharedPrefs.getString("prefDefaultCountry", "0"));
+        		        Log.d(TAG, "Country Code: " + country);
+        		        if( country == 0 ){
+        		        	for (int i=3; i < size - 2; i++) {
+        		        		locationCodes[j] = easMsg[i];
+        		        		Log.d(TAG, "Location Code: " + locationCodes[j]);
 
-            				//Look up fips code in database, return county and state
-        					String fipscode = locationCodes[j].substring(1, 6);
-            				Log.d(TAG, "Looking up county/state for fips code: " + fipscode);
-            				fips = fipsdb.getCountyState(fipscode);
-            				if( fips != null && fips.moveToFirst() ){
-            					Log.d(TAG, "Location: " + fips.getString(fips.getColumnIndex("county")) + ", " + fips.getString(fips.getColumnIndex("state")));
-            					countiesText.append(fips.getString(fips.getColumnIndex("county")) + ", " + fips.getString(fips.getColumnIndex("state")) + "\n");
-            				}
-            				//fips.close();
-        					j++;
-        				}
+        		        		//Look up fips code in database, return county and state
+        		        		String fipscode = locationCodes[j].substring(1, 6);
+        		        		Log.d(TAG, "Looking up county/state for fips code: " + fipscode);
+        		        		fips = fipsdb.getCountyState(fipscode);
+        		        		if( fips != null && fips.moveToFirst() ){
+        		        			Log.d(TAG, "Location: " + fips.getString(fips.getColumnIndex("county")) + ", " + fips.getString(fips.getColumnIndex("state")));
+        		        			countiesText.append(fips.getString(fips.getColumnIndex("county")) + ", " + fips.getString(fips.getColumnIndex("state")) + "\n");
+        		        		}
+        		        		//fips.close();
+        		        		j++;
+        		        	}
+        		        }else if( country ==1 ){
+        		        	for (int i=3; i < size - 2; i++) {
+        		        		locationCodes[j] = easMsg[i];
+        		        		Log.d(TAG, "Location Code: " + locationCodes[j]);
+
+        		        		//Look up fips code in database, return county and state
+        		        		String clccode = locationCodes[j].substring(1, 6);
+        		        		Log.d(TAG, "Looking up region information for clc code: " + clccode);
+        		        		clc = clcdb.getCountyState(clccode);
+        		        		if( clc != null && clc.moveToFirst() ){
+        		        			Log.d(TAG, "Location: " + clc.getString(clc.getColumnIndex("county")) + ", " + clc.getString(clc.getColumnIndex("state")));
+        		        			countiesText.append(clc.getString(clc.getColumnIndex("county")) + ", " + clc.getString(clc.getColumnIndex("state")) + "\n");
+        		        		}
+        		        		//fips.close();
+        		        		j++;
+        		        	}
+        		        }
         				/*
         				 * TTTT Ñ In the format hhmm, using 15 minute increments up to one hour, using 30 minute increments up to six hours,
         				 * and using hourly increments beyond six hours. Weekly and monthly tests sometimes have a 12 hour or greater
